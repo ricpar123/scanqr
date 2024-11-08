7d43c81f9044a3c19a77180f3d28ff16f42472ba
ricpar123 <ricpar123@gmail.com>
2024-11-08 18:11:20 -0300

    version 2

--- js/script.js
+++ js/script.js
@@ -1,30 +1,24 @@
 // script.js file
 
+ 
+
 
 
 const readerDiv = document.getElementById("qr-reader");
-const startReading = document.getElementById("start");
-const stopReading = document.getElementById("stop");
+
 const qrCodeResult = document.getElementById("qr-result");
 
-let qrCodeReader;
 
-startReading.addEventListener("click", () => {
-    qrCodeReader = new Html5Qrcode(readerDiv);
-    qrCodeReader.start({
-        facingMode:"environment",
-        fps:10,
-        qrbox:{ width:250, height:250}}, (decodedText, decodedResult)=>{
-            console.log("Codigo QR leido");
-            console.log("Texto:", decodedText);
-            console.log("Resultado:", decodedResult);
-            qrCodeResult.innerText = decodedText;
-            localStorage.setItem("qrcodeResult", decodedText);
-        }, (error) => {
-            console.log(error);
-        });
-    });
+var htmlQrcode = new Html5QrcodeScanner(
+    "qr-reader", { fps:10, qrbox:300}
+);
 
-    stopReading.addEventListener("click", () => {
-        qrCodeReader.stop();
-    });
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Scan result: ${decodedText}`, decodedResult );
+
+    htmlQrcode.clear();
+
+}
+
+
+htmlQrcode.render(onScanSuccess);
