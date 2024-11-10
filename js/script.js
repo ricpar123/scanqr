3f13ac2a346d2bdf3725db18c4cfc052992c6e9e
ricpar123 <ricpar123@gmail.com>
2024-11-10 18:51:28 -0300

    version 2.0.7

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
 // script.js file
-
+function domReady(fn) { 
+    if ( 
+        document.readyState === "complete" || 
+        document.readyState === "interactive"
+    ) { 
+        setTimeout(fn, 1000); 
+    } else { 
+        document.addEventListener("DOMContentLoaded", fn); 
+    } 
+} 
  
 
-
+/*
 
 const readerDiv = document.getElementById("qr-reader");
 
 const qrCodeResult = document.getElementById("qr-result");
 
 var myqr = [];
+*/
+domReady(function() {
+
+    function onScanSuccess (decodedText, decodedResult) {
+    //    myqr = decodedText;
+    //    window.location.href= "../html/page1.html";
+        myqr = decodedText;
+        window.location.href= '../page1.html';
 
 
+        alert('qrtext: ' + decodedText, decodedResult );
+    }
+        var htmlQrcode = new Html5QrcodeScanner(
+            "qr-reader", { fps:10, qrbox:300}
+        );
 
-
-function onScanSuccess(decodedText, decodedResult) {
-    myqr = decodedText;
-    window.location.href= "../html/page1.html";
-
-    var htmlQrcode = new Html5QrcodeScanner(
-        "qr-reader", { fps:10, qrbox:300}
-    );
-
-    htmlQrcode.render(onScanSuccess);
-
-   
-   
-    htmlQrcode.clear();
-
+        htmlQrcode.render(onScanSuccess);
     
-}
+    });
+
+
+
+
+
+
+
+
 
 
 
